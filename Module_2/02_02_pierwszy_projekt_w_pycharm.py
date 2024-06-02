567ba50d88bf7c497261e2cf0ef284fc7190e231
Pawel Puszka <pawel.puszka@gmail.com>
2024-06-02 22:08:47 +0200

    pierwsze pliki z praktycznymi przykładami

---
+++ Module_2/02_02_pierwszy_projekt_w_pycharm.py
@@ -0,0 +1,12 @@
+name = input("Podaj swoje imię ")
+age = input("Podaj swój wiek ")
+
+print("Masz na imię " + name + " i masz " + age + " lat")
+
+if int(age) >= 18:
+    print("Jesteś pełnoletni(-a)")
+else:
+    print("Jesteś niepełnoletni(-a)")
+
+
+
---
+++ Module_2/Stringi - metody.py
@@ -0,0 +1,10 @@
+game_name = 'Civilization: Through the Ages'
+
+print(game_name.lower())
+print(game_name.upper())
+
+game_name = 'civilization: Through the Ages'
+print(game_name.capitalize())
+
+game_name = '   Civilization: Through the Ages  '
+print(game_name.strip())
