c88d5fb96bfdc63c08ac71c91b4ee2448986e610
Paweł <pawel.puszka@sorigo.pl>
2024-07-05 16:19:42 +0200

    coś było poprawiane

--- Module_3/FILE - writing_to_CSV.py
+++ Module_3/FILE - writing_to_CSV.py
@@ -7,14 +7,13 @@ import csv
     # csv_writer.writerow(['Ania', 25])
 
 with open('test_files/users.csv', 'w') as csv_file:
-    # csv_writer = csv.writer(csv_file)  # obiekt służący do zapisywania danych w pliku CSV
     fieldnames = ['name', 'age']
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     csv_writer.writeheader()
     csv_writer.writerow(
         {
-             'name': 'Adam'
+            'name': 'Adam'
             ,'age': 42
         }
     )
