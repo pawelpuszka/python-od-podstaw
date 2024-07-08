e29df66e14435085331a2cc52dbcdc67840ce91e
Pawel Puszka <pawel.puszka@gmail.com>
2024-07-08 23:54:53 +0200

    funkcja main biblioteki filmów

--- Module_3/movies-library/library.py
+++ Module_3/movies-library/library.py
@@ -3,6 +3,12 @@ import  csv
 def _create_header_() -> list():
     return ['title', 'genre', 'year']
 
+
+def get_csv_reader():
+    with open('data/movies_library.csv') as file:
+        csv_reader = csv.DictReader(file)
+    return csv_reader
+
 def create_library():
     with open('data/movies_library.csv', 'w') as file:
         header = _create_header_()
@@ -18,6 +24,11 @@ def create_movie() -> tuple():
 
 def add_movie(movie: tuple):
     title, genre, year = movie
+    with open('data/movies_library.csv') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['title'] == title:
+                raise NameError("Taki film już jest w bazie danych - dane użytkownika")
     with open('data/movies_library.csv', 'a') as file:
         csv_writer = csv.writer(file)
         csv_writer.writerow([title, genre, year])
@@ -36,22 +47,31 @@ def get_movie_from_file(movie_title: str) -> dict():
     raise NameError("Taki film nie istnieje w bazie - dane użytkownika")
 
 
+def show_movie_info(movie: dict):
+    print(f"Tytuł : {movie['title']}")
+    print(f"Gatunek: {movie['genre']}")
+    print(f"Rok premiery: {movie['year']}")
+    print("*******************************")
+
+
 def show_movies():
     with open('data/movies_library.csv') as file:
         csv_reader = csv.DictReader(file)
 
         for movie in csv_reader:
-            print(f"Tytuł : {movie['title']}")
-            print(f"Gatunek: {movie['genre']}")
-            print(f"Rok premiery: {movie['year']}")
+            show_movie_info(movie)
 
 
-def _get_option_value():
-    return int(input("Wybierz opcję: "))
+def _get_option_value(min: int, max: int) -> int():
+    option = int(input("Wybierz opcję: "))
+    if (option < min) or (option > max):
+        raise ValueError
+
+    return option
 
 
 def edit_movie(movie: dict, option: int):
-    new_movie = movie
+    new_movie = movie.copy()
     match option:
         case 1:
             new_movie['title'] = input("Podaj nowy tytuł: ")
@@ -84,16 +104,17 @@ def _info_(msg: str):
 def main():
     create_library()
 
-    print("1 - Wyświetl filmy")
-    print("2 - Dodaj nowy film")
-    print("3 - Edytuj film")
-    print("4 - Usuń film")
-    print("5 - Wyszukaj film po tytule")
-    print("0 - Wyjście")
-
     while True:
+        _info_("Menu: ")
+        print("1 - Wyświetl filmy")
+        print("2 - Dodaj nowy film")
+        print("3 - Edytuj film")
+        print("4 - Usuń film")
+        print("5 - Wyszukaj film po tytule")
+        print("0 - Wyjście")
+
         try:
-            option = _get_option_value()
+            option = _get_option_value(0, 5)
         except ValueError:
             print("Musisz podać liczbę od 0 - 5")
             continue
@@ -101,54 +122,55 @@ def main():
         match option:
             case 1:
                 _info_("Lista filmów")
+                print("-----------")
                 show_movies()
             case 2:
                 _info_("Dodawanie nowego filmu")
                 movie = create_movie()
-                add_movie(movie)
+                try:
+                    add_movie(movie)
+                except NameError:
+                    print("Taki film już istnieje")
+                    continue
             case 3:
                 while True:
                     _info_("Edycja filmu")
+                    try:
+                        movie_title = get_movie_name_from_user()
+                        movie = get_movie_from_file(movie_title)
+                    except NameError:
+                        print("Nie ma takiego filmu w bazie danych")
+                        continue
                     print("1 - nazwa")
                     print("2 - gatunek")
                     print("3 - rok premiery")
                     try:
-                        option = _get_option_value()
+                        option = _get_option_value(1, 3)
                         break
                     except ValueError:
                         print("Musisz podać liczbę 1 - 3")
                         continue
-
-                movie_title = get_movie_name_from_user()
-                movie = get_movie_from_file(movie_title)
                 edit_movie(movie, option)
             case 4:
                 _info_("Usuwanie filmu")
-                movie_title = get_movie_name_from_user()
+                try:
+                    movie_title = get_movie_name_from_user()
+                    movie = get_movie_from_file(movie_title)
+                except NameError:
+                    print("Nie ma takiego filmu w bazie danych")
+                    continue
                 remove_movie(movie_title)
             case 5:
-                pass
+                try:
+                    movie_title = get_movie_name_from_user()
+                except NameError:
+                    print("Nie ma takiego filmu w bazie danych")
+                    continue
+                movie = get_movie_from_file(movie_title)
+                show_movie_info(movie)
             case 0:
                 _info_("Koniec programu")
                 break
 
 if __name__ == "__main__":
     main()
-    # create_library()
-    #
-    # movie = create_movie()
-    # add_movie(movie)
-    #
-    # movie = create_movie()
-    # add_movie(movie)
-    #
-    # print("Edycja filmu")
-    # movie_title = get_movie_name_from_user()
-    # movie = get_movie_from_file(movie_title)
-    # edit_movie(movie, 2)
-    # show_movies()
-    #
-    # print("Usuwanie filmu")
-    # movie_title = get_movie_name_from_user()
-    # remove_movie(movie_title)
-    # show_movies()
