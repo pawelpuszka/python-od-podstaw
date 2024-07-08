09a098926129bb960a23a054b91e21c2a3c11453
Pawel Puszka <pawel.puszka@gmail.com>
2024-07-08 09:33:25 +0200

    usuwanie filmów - wersja wstępna nie dokończona

--- Module_3/movies-library/library.py
+++ Module_3/movies-library/library.py
@@ -23,17 +23,8 @@ def add_movie(movie: tuple):
         csv_writer.writerow([title, genre, year])
 
 
-def remove_movie(movie_title):
-    with open('data/movies_library.csv') as file:
-        csv_reader = csv.DictReader(file)
-        csv_reader = tuple(csv_reader.vales())
-        create_library()
-        for row in csv_reader:
-
-
-
 def get_movie_name_from_user() -> str():
-    return input("Tytuł filmu do edycji: ")
+    return input("Podaj tytuł filmu: ")
 
 
 def get_movie_from_file(movie_title: str) -> dict():
@@ -45,8 +36,27 @@ def get_movie_from_file(movie_title: str) -> dict():
     raise NameError("Taki film nie istnieje w bazie - dane użytkownika")
 
 
+def show_movies():
+    with open('data/movies_library.csv') as file:
+        csv_reader = csv.DictReader(file)
 
-def edit_movie(movie: dict, option: int) -> tuple():
+        for movie in csv_reader:
+            print(f"Tytuł : {movie['title']}")
+            print(f"Gatunek: {movie['genre']}")
+            print(f"Rok premiery: {movie['year']}")
+
+
+def _read_movies_from_file_() -> list():
+    with open('data/movies_library.csv') as file:
+        movies_list = list()
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            movies_list.append(row)
+        # print(movies_list)
+        return movies_list
+
+
+def edit_movie(movie: dict, option: int):
     new_movie = movie
     match option:
         case 1:
@@ -56,7 +66,41 @@ def edit_movie(movie: dict, option: int) -> tuple():
         case 3:
             new_movie['year'] = input("Podaj nowy rok premiery: ")
 
-    return tuple(new_movie.values())
+    movies_list = _read_movies_from_file_()
+
+    for movie in movies_list:
+        if movie['title'] == new_movie['title']:
+            movies_list = new_movie
+
+    with open('data/movies_library.csv') as file:
+        csv_writer = csv.writer(file)
+        create_library()
+        for movie in movies_list:
+            add_movie(tuple(movie.values())) !!!!!!!!!!!!!!!!
+
+
+
+def remove_movie(movie_title: str):
+    with open('data/movies_library.csv') as file:
+        csv_reader = csv.DictReader(file)
+        create_library()
+        for row in csv_reader:
+            if row['title'] != movie_title:
+                add_movie(tuple(row.values()))
+
+
+# def main():
+#     while True:
+#         option = int(input("Wybierz opcję: "))
+#         print("1 - Wyświetl filmy")
+#         print("Dodaj nowy film")
+#         print("Edytuj film")
+#         print("Usuń film")
+#         print("Wyszukaj film po tytule")
+#
+#         match option:
+#             case 1:
+
 
 
 
@@ -66,5 +110,9 @@ if __name__ == "__main__":
     add_movie(movie)
     movie_title = get_movie_name_from_user()
     movie = get_movie_from_file(movie_title)
-    movie = edit_movie(movie, 2)
-    print(movie)
+    edit_movie(movie, 2)
+    show_movies()
+    # print(movie)
+    # print("Usuwanie filmu")
+    # movie_title = get_movie_name_from_user()
+    # remove_movie(movie_title)
