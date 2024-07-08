c76ffacfcfd796b4463675881aefc70edddf71f2
Paweł <pawel.puszka@sorigo.pl>
2024-07-08 11:15:47 +0200

    funkcja main

--- Module_3/movies-library/library.py
+++ Module_3/movies-library/library.py
@@ -46,14 +46,8 @@ def show_movies():
             print(f"Rok premiery: {movie['year']}")
 
 
-def _read_movies_from_file_() -> list():
-    with open('data/movies_library.csv') as file:
-        movies_list = list()
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            movies_list.append(row)
-        # print(movies_list)
-        return movies_list
+def _get_option_value():
+    return int(input("Wybierz opcję: "))
 
 
 def edit_movie(movie: dict, option: int):
@@ -66,53 +60,95 @@ def edit_movie(movie: dict, option: int):
         case 3:
             new_movie['year'] = input("Podaj nowy rok premiery: ")
 
-    movies_list = _read_movies_from_file_()
-
-    for movie in movies_list:
-        if movie['title'] == new_movie['title']:
-            movies_list = new_movie
-
-    with open('data/movies_library.csv') as file:
-        csv_writer = csv.writer(file)
-        create_library()
-        for movie in movies_list:
-            add_movie(tuple(movie.values())) !!!!!!!!!!!!!!!!
-
+    remove_movie(movie['title'])
+    add_movie(tuple(new_movie.values()))
 
 
 def remove_movie(movie_title: str):
+    movies_list = list()
     with open('data/movies_library.csv') as file:
         csv_reader = csv.DictReader(file)
-        create_library()
         for row in csv_reader:
+            movies_list.append(row)
+
+        create_library()
+        for row in movies_list:
             if row['title'] != movie_title:
                 add_movie(tuple(row.values()))
 
 
-# def main():
-#     while True:
-#         option = int(input("Wybierz opcję: "))
-#         print("1 - Wyświetl filmy")
-#         print("Dodaj nowy film")
-#         print("Edytuj film")
-#         print("Usuń film")
-#         print("Wyszukaj film po tytule")
-#
-#         match option:
-#             case 1:
+def _info_(msg: str):
+    print()
+    print(msg)
 
+def main():
+    create_library()
 
+    print("1 - Wyświetl filmy")
+    print("2 - Dodaj nowy film")
+    print("3 - Edytuj film")
+    print("4 - Usuń film")
+    print("5 - Wyszukaj film po tytule")
+    print("0 - Wyjście")
 
+    while True:
+        try:
+            option = _get_option_value()
+        except ValueError:
+            print("Musisz podać liczbę od 0 - 5")
+            continue
+
+        match option:
+            case 1:
+                _info_("Lista filmów")
+                show_movies()
+            case 2:
+                _info_("Dodawanie nowego filmu")
+                movie = create_movie()
+                add_movie(movie)
+            case 3:
+                while True:
+                    _info_("Edycja filmu")
+                    print("1 - nazwa")
+                    print("2 - gatunek")
+                    print("3 - rok premiery")
+                    try:
+                        option = _get_option_value()
+                        break
+                    except ValueError:
+                        print("Musisz podać liczbę 1 - 3")
+                        continue
+
+                movie_title = get_movie_name_from_user()
+                movie = get_movie_from_file(movie_title)
+                edit_movie(movie, option)
+            case 4:
+                _info_("Usuwanie filmu")
+                movie_title = get_movie_name_from_user()
+                remove_movie(movie_title)
+            case 5:
+                pass
+            case 0:
+                _info_("Koniec programu")
+                break
 
 if __name__ == "__main__":
-    create_library()
-    movie = create_movie()
-    add_movie(movie)
-    movie_title = get_movie_name_from_user()
-    movie = get_movie_from_file(movie_title)
-    edit_movie(movie, 2)
-    show_movies()
-    # print(movie)
+    main()
+    # create_library()
+    #
+    # movie = create_movie()
+    # add_movie(movie)
+    #
+    # movie = create_movie()
+    # add_movie(movie)
+    #
+    # print("Edycja filmu")
+    # movie_title = get_movie_name_from_user()
+    # movie = get_movie_from_file(movie_title)
+    # edit_movie(movie, 2)
+    # show_movies()
+    #
     # print("Usuwanie filmu")
     # movie_title = get_movie_name_from_user()
     # remove_movie(movie_title)
+    # show_movies()
