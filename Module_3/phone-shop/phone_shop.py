12106031b03e16be90cb91c3383b9e2a6d070708
Paweł <pawel.puszka@sorigo.pl>
2024-08-07 23:08:25 +0200

    funkcja modyfikujaca telefon

--- Module_3/phone-shop/phone_shop.py
+++ Module_3/phone-shop/phone_shop.py
@@ -35,6 +35,60 @@ def remove_phone_from_list_by_name(phone_list: list, phone_name: str):  # rzuca 
     for index, phone in enumerate(phone_list):
         if phone['name'] == phone_name:
             del phone_list[index]
+        else:
+            raise ValueError("Nie znaleziono telefonu")
+
+
+def display_stock_list(phone_list: list):
+    for phone in phone_list:
+        print("Nazwa telefonu: " + phone['name'])
+        print("Specyfikacja:")
+        print("    Kolor : " + phone['specifications']['color'])
+        print("    Pamięć: " + str(phone['specifications']['memory']))
+        print("Cena: " + str(phone['price']))
+        print()
+
+
+def display_stock_file():
+    with open('phones.json') as phones_file:
+        phones = json.load(phones_file)
+    for phone in phones:
+        print("Nazwa telefonu: " + phone['name'])
+        print("Specyfikacja:")
+        print("    Kolor : " + phone['specifications']['color'])
+        print("    Pamięć: " + str(phone['specifications']['memory']))
+        print("Cena: " + str(phone['price']))
+        print()
+
+
+def _get_phone_by_name_(phone_list: list, phone_name: str) -> dict():
+    tmp_phone = dict()
+    for phone in phone_list:
+        if phone_name == phone['name']:
+            tmp_phone = phone
+
+    return phone
+
+
+def modify_phone(phone_list: list):
+    print("Który telefon chcesz zmodyfikować. Podaj nazwę.")
+    phone_name = get_phone_name()
+    phone = _get_phone_by_name_(phone_list, phone_name)
+    print("Co chcesz zmienić")
+    print("1 - kolor")
+    print("2 - pamięć")
+    print("3 - cena")
+    option = input("Podaj opcję: ")
+
+    match option:
+        case 1:
+            phone['color'] = input("Podaj nowy kolor: ")
+        case 2:
+            phone['memory'] = input("Podaj nową wartość dla pamięci: ")
+        case 3:
+            phone['price'] = input("Podaj nową cenę: ")
+
+
 
 def put_list_into_file(phone_list: list):
     pass
@@ -49,6 +103,8 @@ if __name__ == "__main__":
     # print(phones)
 
     # removing
-    phone_name = get_phone_name()
-    remove_phone_from_list_by_name(phones, phone_name)
-    print(phones)
+    # phone_name = get_phone_name()
+    # remove_phone_from_list_by_name(phones, phone_name)
+    # print(phones)
+    display_stock_list(phones)
+    display_stock_file()
